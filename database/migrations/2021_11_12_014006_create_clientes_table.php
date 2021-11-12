7f4fc316a1293032f5ef36dd1fc39265d45ddc38
Kalcio <mmunozm@utem.cl>
2021-11-12 00:30:24 -0300

    Tabla cliente creada

---
+++ database/migrations/2021_11_12_014006_create_clientes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 30);
+            $table->integer('numero');
+            $table->string('email', 20);
+            $table->string('direccion', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
