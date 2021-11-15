506830ff2db22df6abb989ca0490bd829fa0bb51
Kalcio <mmunozm@utem.cl>
2021-11-15 18:24:43 -0300

    Favicon creado y barra de búsqueda borrada

--- config/adminlte.php
+++ config/adminlte.php
@@ -31,7 +31,7 @@ return [
     */
 
     'use_ico_only' => false,
-    'use_full_favicon' => false,
+    'use_full_favicon' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -47,8 +47,8 @@ return [
 
     'logo' => '<b>EURO</b>TRANS',
     'logo_img' => 'vendor/adminlte/dist/img/LogoWhite_E.png',
-    'logo_img_class' => 'brand-image img-circle elevation-3',
-    'logo_img_xl' => null,
+    'logo_img_class' => 'img-circle',
+    'logo_img_xl' => 'vendor/adminlte/dist/img/Logobig_white.png',
     'logo_img_xl_class' => 'brand-image-xs',
     'logo_img_alt' => 'AdminLTE',
 
@@ -229,7 +229,7 @@ return [
         [
             'type'         => 'navbar-search',
             'text'         => 'search',
-            'topnav_right' => true,
+            'topnav_right' => false,
         ],
         [
             'type'         => 'fullscreen-widget',
