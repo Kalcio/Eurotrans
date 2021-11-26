c586d82273866c952dcaa7580f7d2f1633767891
LDonGithub <ldonoso@utem.cl>
2021-11-26 20:13:03 -0300

    Agrego Vista y Funcionamiento Ruta

---
+++ resources/views/ruta/create.blade.php
@@ -0,0 +1,33 @@
+@extends('adminlte::page')
+
+@section('title', 'Crear Ruta')
+
+@section('content_header')
+    <h1>Crear Ruta</h1>
+@stop
+
+@section('content')
+
+<form action="/rutas" method="POST">
+    @csrf
+    <div class="mb-3">
+        <label for="" class="form-label">Origen</label>
+        <input id="origen" name="origen" type="text" class="form-control" tabindex="1">
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label">Destino</label>
+        <input id="destino" name="destino" type="tel" class="form-control" tabindex="2">
+    </div>
+    <a href="/rutas" class="btn btn-secondary" tabindex="5">Cancelar</a>
+    <button type="submit" class="btn btn-primary" tabindex="6">Guardar</button>
+</form>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+    <link rel="icon" type="image/png" sizes="57x57" href="{{ asset('favicons/Logo_E.png') }}">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
