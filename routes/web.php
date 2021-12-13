8917e9fe5f65c16ecc4611fd32d3a5d616c0bfe1
Kalcio <mmunozm@utem.cl>
2021-12-13 13:14:12 -0300

    Eliminación registro

--- routes/web.php
+++ routes/web.php
@@ -35,13 +35,13 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dash', function () {
     return view('dash.index');
 })->name('dash');
 
-Route::get('/dash/crud', function() {
-    return view('crud.index');
-});
+// Route::get('/dash/crud', function() {
+//     return view('crud.index');
+// });
 
-Route::get('/dash/crud/create', function() {
-    return view('crud.create');
-});
+// Route::get('/dash/crud/create', function() {
+//     return view('crud.create');
+// });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
